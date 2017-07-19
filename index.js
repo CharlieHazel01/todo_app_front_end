f940c3fd97d0c0522ddd7d237e44e1c796866ea3
Charlie Hazel <hazel.charlie@gmail.com>
2017-07-19 19:54:30 +0000

    newest java

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     $("#submit-button").click(function(){
-    var todoBlock=$(this).closest(#new-todo-block)
-    var description$(this).find(#new-task-box)
+    var todoBlock=$(this).closest("#new-todo-block");
+    var description=$(this).find("#new-task-box");
     
-    $("ul").append("<li><input type='checkbox'/>") + description.val() + "<span class='todo'>""</span></li>")
+    $("ul").append("<li><input type='checkbox'/><span class='todo'><a href='show.html'>" + description.val() + "</a></span></li>");
     
-    description.val("")
+    description.val("");
     
     })
 })
