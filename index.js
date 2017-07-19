7ba3620c0dcf72bec155b883269282d60f4e3b74
Charlie Hazel <hazel.charlie@gmail.com>
2017-07-19 17:47:49 +0000

    Java

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#submit-button").click(function(){
+    var todoBlock=$(this).closest(#new-todo-block)
+    var description$(this).find(#new-task-box)
+    
+    $("ul").append("<li><input type='checkbox'/>") + description.val() + "<span class='todo'>""</span></li>")
+    
+    
+    description.val("")
+    
+    
+    })
+})
