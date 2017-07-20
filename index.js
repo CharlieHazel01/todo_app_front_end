61c2b4bf78c880197bcb3a02d3bc075c226e9032
Charlie Hazel <hazel.charlie@gmail.com>
2017-07-20 01:49:30 +0000

    LateNight New

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
     $("#submit-button").click(function(){
     var todoBlock=$(this).closest("#new-todo-block");
-    var description=$(this).find("#new-task-box");
+    var description=todoBlock.find("#new-task-box");
     
     $("ul").append("<li><input type='checkbox'/><span class='todo'><a href='show.html'>" + description.val() + "</a></span></li>");
     
     description.val("");
     
     })
+    $(document).ready(function(){
+        $("#")
+    }
 })
